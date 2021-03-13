ac2ddb4a2bd7b4afbb761395ebf181d6479d2040
GabrielFDias <g.dias_007@alunos.fho.edu.br>
2021-03-13 18:22:38 -0300

    Atividades da semana 6, 115 testes concluídos

---
+++ BancoNapp/contas/Conta.py
@@ -0,0 +1,52 @@
+class Conta:
+    def __init__(self, **kwargs):
+        """
+        Construtor da classe Conta.
+        Recebe por kwargs :
+        - nome
+        - limite
+        - saldo
+
+        Raises
+            ValueError: Caso o saldo seja menor ou igual a zero.
+        """
+        self.extrato = []
+        self.limite = kwargs.get('limite', 500)
+        self.nome = kwargs.get('nome', None)
+        self.saldo = 0
+        saldo = kwargs.get('saldo', self.saldo)
+        if saldo < 0:
+            raise ValueError('Saldo negativo')
+        self.saldo = saldo
+        self.extrato.append(('I', saldo))
+
+
+    def get_extrato(self):
+        """
+        Retorna a lista dos saques e depósitos feitos na conta.
+
+        Returns:
+            List: Lista de operações
+        """
+        return self.extrato
+
+    
+    def deposito(self, valor):
+        """
+        Método para realizar depósito.
+        Este método suporta somente números maiores que zero.
+
+        Args:
+            valor (float ou int): Valor positivo do depósito
+
+        Raises:
+            ValueError: Erro ocorre quando é informado valor negativo.
+            TypeError: Quando o tipo passado não for inteiro ou float.
+        """
+        if isinstance(valor, (float, int)):
+            if valor <= 0:
+                raise ValueError('Valor do depósito precisa ser maior que zero')
+            self.saldo = self.saldo + valor
+            self.extrato.append(('D', valor))
+            return
+        raise TypeError('O depósito precisa ser numérico')
---
+++ BancoNapp/contas/ContaPessoaJuridica.py
@@ -0,0 +1,37 @@
+from BancoNapp.contas.Conta import Conta
+
+
+class ContaPessoaJuridica(Conta):
+    def __init__(self,  **kwargs):
+        """
+        Construtor da classe PessoaJuridica
+        Extrai do dicionário kwargs a empresa do correntista.
+        """
+        self.empresa = kwargs.get('empresa', '')
+        super(ContaPessoaJuridica, self).__init__(**kwargs)
+        self.limite = kwargs.get('limite', 1500)
+
+
+    def saque(self, valor):
+        """
+        Método para realizar saque.
+        Este método suporta somente números maiores que zero.
+
+        Args:
+            valor (float ou int): Valor positivo do saque
+
+        Raises:
+            ValueError: Erro ocorre quando é informado valor negativo.
+            TypeError: Quando o tipo passado não for inteiro ou float.
+
+        Returns:
+            Float: Valor do saque realizado.
+        """
+        if isinstance(valor, (float, int)):
+            if valor > (self.saldo + self.limite):
+                raise ValueError('Valor do saque supera seu saldo e seu limite')
+                return
+            self.saldo = self.saldo - valor
+            self.extrato.append(('S', valor))
+            return valor
+        raise TypeError('O valor do saque precisa ser numérico')
